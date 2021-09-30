70ebe6a7a74c3042e14ef5447d0c483f0d061c96
Dieumegard <dieumegard.gregory@gmail.com>
2021-09-30 21:26:59 +0200

    modifications sur la page tools

--- page2.js
+++ page2.js
@@ -1,11 +1,9 @@
 function show(x) {
     document.querySelector('.hide' + x).classList.add('show');
-    document.querySelector('.gabarit' + x).style.opacity = '0';
     document.querySelector('.button' + x).style.opacity = '0';
 };
 function close(x) {
     document.querySelector('.hide' + x).classList.remove('show');
-    document.querySelector('.gabarit' + x).style.opacity = '1';
     document.querySelector('.button' + x).style.opacity = '1';
 }
 document.querySelector('.close1').addEventListener('click', function() {
