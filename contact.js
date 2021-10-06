d5d594fc19f5e181fa569c370f0ee2624941ffe9
Dieumegard <dieumegard.gregory@gmail.com>
2021-10-06 10:23:22 +0200

    finalisation de la page contact (html/css/js + logo)

--- contact.js
+++ contact.js
@@ -1,10 +1,19 @@
+const burger = document.getElementById('burger');
+const navMenu = document.querySelector('.navmenu');
+//Evènement pour faire apparaitre le menu
+burger.addEventListener('click', () => {
+    navMenu.classList.toggle('open');
+})
 document.getElementById('form').addEventListener('submit', function(event) {
     document.getElementById('open-modal').style.display = "block";
+    document.querySelector('header').style.display = "none";
     event.preventDefault();
     
 });
     document.getElementById('close').addEventListener('click', function() {
         document.getElementById('open-modal').style.display = "none";
+        document.querySelector('header').style.display = "flex";
     });
+
     
 
